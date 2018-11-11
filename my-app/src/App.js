ec4e7026e078fb2d57c3f56c429985a6b625c06d
jayseeg <jgibbons@splunk.com>
2018-11-11 16:41:43 -0700

    setup UI skeleton

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,23 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  static defaultProps = {
+    quadrants: [
+      0,
+      1,
+      2,
+      3,
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hi
-          </a>
-        </header>
+        {this.props.quadrants.map(val => (
+          <div className={'quadrant quadrant-' + val} />
+        ))}
       </div>
     );
   }
